bb9ba60d875aaad26549ff3801b69462c6cac1c9
Jessica <44973365+JessicaLBarbosa@users.noreply.github.com>
2020-05-04 13:31:29 -0300

    Update requisicaoAjax.js

--- js/requisicaoAjax.js
+++ js/requisicaoAjax.js
@@ -1,25 +1,28 @@
-function enviar(){
-		
-		tituloFilme = document.getElementById("titulo").innerHTML;
-		idFilme = document.getElementById("idFilme").value;
+function enviar () {
+  tituloFilme = document.getElementById ('titulo').innerHTML;
+  idFilme = document.getElementById ('idFilme').value;
 
-		var ajax = new XMLHttpRequest();
+  var ajax = new XMLHttpRequest ();
 
-		// Seta tipo de requisição: Post e a URL da API
-		ajax.open("POST", "http://localhost/mistermoveis/php/enviarRequisicaoBd.php", true);
-		ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // Seta tipo de requisição: Post e a URL da API
+  ajax.open (
+    'POST',
+    'http://localhost/moviesmaster/php/enviarRequisicaoBd.php',
+    true
+  );
+  ajax.setRequestHeader ('Content-type', 'application/x-www-form-urlencoded');
 
-		// Seta paramêtros da requisição e envia a requisição
-		ajax.send('titulo=' + tituloFilme + '&idFilme= ' + idFilme);
+  // Seta paramêtros da requisição e envia a requisição
+  ajax.send ('titulo=' + tituloFilme + '&idFilme= ' + idFilme);
 
-		// Cria um evento para receber o retorno.
-		ajax.onreadystatechange = function() {
-		  // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
-			if(ajax.readyState == 4 && ajax.status == 200) {
-				var data = ajax.responseText;
-		    // Retorno do Ajax
-				console.log(data);
-				alert("Enviado com sucesso!");
-			}
-		}
+  // Cria um evento para receber o retorno.
+  ajax.onreadystatechange = function () {
+    // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      var data = ajax.responseText;
+      // Retorno do Ajax
+      console.log (data);
+      alert ('Enviado com sucesso!');
+    }
+  };
 }
